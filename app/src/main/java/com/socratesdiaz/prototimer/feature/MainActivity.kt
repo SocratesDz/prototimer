8158957e700a4c652c7abd28a50d3b8fc81a1c5e
Sócrates Díaz <sdsxd1800@gmail.com>
2018-11-24 20:16:28 -0400

    * Created TaskDetailBottomSheetFragment. * Added support for bottom sheet dialog in MainActivity. * Created mock designs of bottom sheet task detail.

--- app/src/main/java/com/socratesdiaz/prototimer/feature/MainActivity.kt
+++ app/src/main/java/com/socratesdiaz/prototimer/feature/MainActivity.kt
@@ -2,14 +2,22 @@ package com.socratesdiaz.prototimer.feature
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.view.ViewGroup
 import com.socratesdiaz.prototimer.R
 import com.socratesdiaz.prototimer.feature.search.ui.SearchFragment
+import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.task_detail_content_bottom_sheet.*
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setupViews()
+        showPeekTaskDetailBottomSheet()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, SearchFragment.newInstance())
@@ -17,4 +25,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupViews() {
+        taskDetailBottomSheetTitle.setOnClickListener {
+            expandTaskDetailBottomSheet()
+        }
+    }
+
+    private fun showPeekTaskDetailBottomSheet() {
+        val taskBottomSheetBehavior = BottomSheetBehavior.from(taskDetailBottomSheet)
+        taskBottomSheetBehavior.peekHeight = (resources.displayMetrics.density * 64).roundToInt()
+        taskBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+    }
+
+    private fun expandTaskDetailBottomSheet() {
+        val taskBottomSheetBehavior = BottomSheetBehavior.from(taskDetailBottomSheet)
+        taskBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
 }
---
+++ app/src/main/java/com/socratesdiaz/prototimer/feature/taskdetail/ui/TaskDetailBottomSheetFragment.kt
@@ -0,0 +1,11 @@
+package com.socratesdiaz.prototimer.feature.taskdetail.ui
+
+import android.app.Dialog
+import android.os.Bundle
+import android.support.design.widget.BottomSheetDialogFragment
+
+class TaskDetailBottomSheetFragment: BottomSheetDialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
